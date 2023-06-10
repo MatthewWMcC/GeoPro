e278f3ee59844ab5ab5ffcc2466088480abd053a
Matthew McCracken <matthewwmccracken@gmail.com>
2023-06-09 23:55:04 -0400

    deploy

--- src/services/keys/firestore.key.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyAuuMMJ7EDPi0BkhkLU8amSZLKG5uIGAHA",
-  authDomain: "geopro-324602.firebaseapp.com",
-  projectId: "geopro-324602",
-  storageBucket: "geopro-324602.appspot.com",
-  messagingSenderId: "985880821943",
-  appId: "1:985880821943:web:c695d8222dcf116b6d4d32",
-};
