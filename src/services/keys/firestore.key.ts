6d9c1e41381428308ff15954a929f21b90d76cf6
Matthew McCracken <matthewwmccracken@gmail.com>
2023-06-11 19:55:43 -0400

    firestore key changes

---
+++ src/services/keys/firestore.key.ts
@@ -0,0 +1,10 @@
+const envVars = process.env.parsed;
+
+export const firebaseConfig = {
+  apiKey: envVars.FIRESTORE_API_KEY,
+  authDomain: envVars.FIRESTORE_AUTH_DOMAIN,
+  projectId: envVars.FIRESTORE_PROJECT_ID,
+  storageBucket: envVars.FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: envVars.FIRESTORE_MESSAGING_SENDER_ID,
+  appId: envVars.FIRESTORE_APP_ID,
+};
