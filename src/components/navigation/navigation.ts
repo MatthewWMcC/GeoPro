f6c95ba85d9de3e5ed579626e88f6db6799b1ed1
Matthew McCracken <matthewwmccracken@gmail.com>
2023-07-05 22:31:10 -0400

    fixed nav maps

--- src/components/navigation/navigation.ts
+++ src/components/navigation/navigation.ts
@@ -3,7 +3,6 @@ import { NavigationAttrs, NavigationState } from "./types";
 import "./navigation.css";
 import { rightArrowSVG, leftArrowSVG } from "global/constants/icons";
 import { model } from "./navigation-model";
-import { Pages } from "state/CurrentPageState/types";
 import { mapOptions } from "./constants";
 
 export const navigation: m.Component<NavigationAttrs, NavigationState> = {
@@ -11,48 +10,45 @@ export const navigation: m.Component<NavigationAttrs, NavigationState> = {
   oninit: model.handleComponentInit,
   oncreate: model.handleComponentCreate,
   view: (vnode: m.VnodeDOM<NavigationAttrs, NavigationState>): m.Children => {
-    const { navShown, CurrentPage, preferedMapStyle } = vnode.state;
+    const { navShown, preferedMapStyle } = vnode.state;
 
-    return (
-      CurrentPage !== Pages.LOGIN &&
-      m(`.navigation-head-container${navShown ? "" : ".closed"}`, [
-        m(".nav-main-container", [
-          m("h4#map-style-label", "Map Style"),
-          m("#map-options", [
-            mapOptions.map(({ id, label, mapType }) => {
-              return m(
-                `.map-option${mapType === preferedMapStyle ? ".selected" : ""}`,
-                { onclick: () => model.handleMapStyleChange(mapType) },
-                [
-                  m(`.map-option-image-container#${id}`),
-                  m("label#map-style-label", label),
-                ]
-              );
-            }),
-          ]),
+    return m(`.navigation-head-container${navShown ? "" : ".closed"}`, [
+      m(".nav-main-container", [
+        m("h4#map-style-label", "Map Style"),
+        m("#map-options", [
+          mapOptions.map(({ id, label, mapType }) => {
+            return m(
+              `.map-option${mapType === preferedMapStyle ? ".selected" : ""}`,
+              { onclick: () => model.handleMapStyleChange(mapType) },
+              [
+                m(`.map-option-image-container#${id}`),
+                m("label#map-style-label", label),
+              ]
+            );
+          }),
         ]),
-        m(".nav-show-hide-button-container", [
-          m(
-            "button.nav-show-hide-button",
-            {
-              onclick: () => {
-                if (navShown) {
-                  vnode.state.navShown = false;
-                  m.redraw();
-                } else {
-                  vnode.state.navShown = true;
-                  m.redraw();
-                }
-              },
+      ]),
+      m(".nav-show-hide-button-container", [
+        m(
+          "button.nav-show-hide-button",
+          {
+            onclick: () => {
+              if (navShown) {
+                vnode.state.navShown = false;
+                m.redraw();
+              } else {
+                vnode.state.navShown = true;
+                m.redraw();
+              }
             },
-            [
-              m(".nav-show-hide-message", [
-                navShown ? m.trust(leftArrowSVG) : m.trust(rightArrowSVG),
-              ]),
-            ]
-          ),
-        ]),
-      ])
-    );
+          },
+          [
+            m(".nav-show-hide-message", [
+              navShown ? m.trust(leftArrowSVG) : m.trust(rightArrowSVG),
+            ]),
+          ]
+        ),
+      ]),
+    ]);
   },
 };
